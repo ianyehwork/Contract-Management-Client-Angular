96cd8341c39460a66c3e330341afe5dbc66379e9
TSU HSIN YEH <tyeh3@IDHFJSNF.local>
2018-11-03 21:26:00 +0800

    Added style and sorting to payment-table

--- src/app/admin/dashboard/components/active-payment-table/active-payment-table.component.ts
+++ src/app/admin/dashboard/components/active-payment-table/active-payment-table.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Contract } from '../../../contract/models/contract';
 import { ContractEditComponent } from '../../../contract/components/contract-edit/contract-edit.component';
 import { AppConstants } from '../../../../constants';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date';
 
 
 @Component({
@@ -16,11 +17,17 @@ export class ActivePaymentTableComponent implements OnInit {
   modelList: Contract[] = [];
 
   constructor(private service: ContractTableService,
-              private modalService: NgbModal) { }
+              private modalService: NgbModal,
+              private calendar: NgbCalendar) { }
 
   ngOnInit() {
     this.service.getContracts().subscribe(contracts => {
         this.modelList = contracts;
+        this.modelList.sort((a, b) => {
+          const d1 = new Date(a.pYear, a.pMonth, a.pDay);
+          const d2 = new Date(b.pYear, b.pMonth, b.pDay);
+          return d1.getDate() - d2.getDate();
+        });
       }
     );
   }
@@ -34,5 +41,25 @@ export class ActivePaymentTableComponent implements OnInit {
     const modalRef = this.modalService.open(ContractEditComponent, AppConstants.MODAL_OPTIONS);
     // Pass poster as a Input to ModalRef
     modalRef.componentInstance.model = model;
+  }
+
+  // Return true if the Contract Payment Date is Today
+  isToday(model: Contract) {
+    const date = this.calendar.getToday();
+    return date.year === model.pYear &&
+           date.month === model.pMonth &&
+           date.day === model.pDay;
+  }
+
+  isPastDue(model: Contract) {
+    const date = this.calendar.getToday();
+    const date2 = new NgbDate(model.pYear, model.pMonth, model.pDay);
+    return date.after(date2);
+  }
+
+  isFuture(model: Contract) {
+    const date = this.calendar.getToday();
+    const date2 = new NgbDate(model.pYear, model.pMonth, model.pDay);
+    return date.before(date2);
   }
 }
