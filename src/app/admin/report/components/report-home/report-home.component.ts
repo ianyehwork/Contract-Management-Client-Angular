35000bbba9cd69b9f7865e2888af31c9788f44e1
TSU HSIN YEH <tyeh3@IDHFJSNF.local>
2018-11-13 19:20:08 +0800

    Added filters to payment report generation.

--- src/app/admin/report/components/report-home/report-home.component.ts
+++ src/app/admin/report/components/report-home/report-home.component.ts
@@ -1,5 +1,6 @@
 import { ReportService } from '../../services/report.service';
 import { Component, OnInit } from '@angular/core';
+import { NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-report-home',
@@ -8,17 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportHomeComponent implements OnInit {
 
-  constructor(private reportService: ReportService) { }
+  constructor(
+    private reportService: ReportService,
+    private calendar: NgbCalendar) { }
+
+  noPaymentFilter;
+  paymentYear;
+  paymentMonth;
+
+  incomeStartDate;
+  incomeEndDate;
 
   ngOnInit() {
+    const today = this.calendar.getToday();
+    this.paymentYear = today.year;
+    this.paymentMonth = today.month;
+    this.noPaymentFilter = true;
   }
 
   // in Chrome and Firefox, with the uBlock Origin extension,
   // it would try to open the new window and close it automatically.
-  createReport() {
-    this.reportService.getPaymentReport().subscribe((response) => {
+  createPaymentReport() {
+    console.log(this.paymentYear);
+    console.log(this.paymentMonth);
+    let query = '';
+    if (!this.noPaymentFilter) {
+      query = `?paymentYear=${this.paymentYear}&paymentMonth=${this.paymentMonth}`;
+    }
+    this.reportService.getPaymentReport(query).subscribe((response) => {
       const fileURL = URL.createObjectURL(response);
       window.open(fileURL);
     });
+  }
+
+  createIncomeReport() {
+    console.log(this.incomeStartDate);
+    console.log(this.incomeEndDate);
   }
 }
